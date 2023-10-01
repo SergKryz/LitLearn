345f9b6e1538eb1ea1b8338a646e7fc31f84a383
Serg K <serg.kryz@gmail.com>
2023-10-01 12:48:32 +0100

    Adjusted aome css

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "../styles/Quiz.css";
 import TopNav from "./TopNav";
+import { useNavigate } from "react-router-dom";
 
 const Quiz = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/");
+  };
   return (
     <div>
       <TopNav />
 
       <div className="quiz-container">
         <div className="quiz-top-nav">
-          <button className="back-button">&larr; Go Back</button>
+          <button className="back-button" onClick={handleBack}>
+            &larr;
+          </button>
         </div>
         <div className="quiz-content">
           <h1>Welcome to the Quiz!</h1>
